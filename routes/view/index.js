5839f25d7d663d308b85bea94ad3cee90331050e
Jonathan Morales <jjm15c@my.fsu.edu>
2018-02-22 16:45:39 -0500

    add root route

--- routes/view/index.js
+++ routes/view/index.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 
+router.get('/', function(req,res){
+  let userSession = req.session;
+  if (!userSession.user) {
+    res.redirect('/login')
+  } else {
+    res.render('home')
+  }
+})
+
+
 router.get("/login", function(req, res) {
   res.render("signIn");
 });
