4b88fb851f9f4f48c7de8a51457b574955a62fef
Jonathan Morales <jjm15c@my.fsu.edu>
2018-02-23 01:42:22 -0500

    Clean frontend and serve css for each page on express response

--- routes/view/index.js
+++ routes/view/index.js
@@ -5,17 +5,17 @@ router.get('/', function(req,res){
   if (!userSession.user) {
     res.redirect('/login')
   } else {
-    res.render('home')
+    res.render('home', { title: 'Sample RESTful WebApp', css: ['home.css'] });
   }
 })
 
 
 router.get("/login", function(req, res) {
-  res.render("signIn");
+   res.render('signIn', { title: 'Sign in', css: ['signin.css'] });
 });
 
 router.get("/register", function(req, res) {
-  res.render("register");
+  res.render('register', { title: 'Register', css: ['register.css'] });
 });
 
 module.exports = router;
